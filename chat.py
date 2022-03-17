d9296a6350030673fcd4eaac62bf5827d2a77e5a
deepakpawade <38484434+deepakpawade@users.noreply.github.com>
2022-03-17 22:13:58 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ import copy
 import sys
 import requests
 from time import sleep
-import praw
+
 TOKEN = 'OTUzOTQzNjQyMTY5NTQwNjU4.YjL7Pw.YjP3hMMHpmI64b0qcjsr5wlwrSY'
 SUB = 'https://www.reddit.com/r/worldnews/comments/'
 
